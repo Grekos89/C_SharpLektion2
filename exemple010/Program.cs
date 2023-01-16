06c55b894007dab73d9b5c0d02760d50811e0e4a
Евгений <Grekos_89@mail.ru>
2023-01-16 15:17:23 +0300

    array2

---
+++ exemple010/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {21, 24, 53,19, 95, 63, 75, 78, 19};
+
+int n = array.Length;
+int find = 19;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find )
+    {
+        System.Console.WriteLine(index);
+        break;
+    }
+    index++ ; // index = index + 1
+}
